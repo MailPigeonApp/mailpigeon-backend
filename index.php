d1bde46f261e683f8c01a6231bebc472eb663682
Desmond Sofua <mrsofua@gmail.com>
2023-07-31 22:24:02 +0000

    feat: added most of api

--- index.php
+++ index.php
@@ -28,6 +28,122 @@ app()->group('/v1', function(){
 
 			response()->json($projects);
 		});
+
+		app()->post('/getProject', function(){
+			$request = request()->get(['id']);
+
+			$project = db()
+				->select('project')
+				->find($request['id']);
+
+			$project['fields'] = json_decode($project['fields'], true);
+
+			response()->json($project);
+		});
+
+		app()->post('/create', function(){
+			$request = request()->get(['name', 'prefix', 'ownerId']);
+
+			$project = db()
+				->insert('project')
+				->params(
+					[
+						"name" => $request['name'],
+						"prefix" => $request['prefix'],
+						'"ownerId"' => $request['ownerId'],
+						"fields" => json_encode([])
+					]
+				)
+				->execute();
+
+			$lastId = db()
+				->select('project', 'id')
+				->where('"ownerId"', $request['ownerId'])
+				->orderBy("created_at", "desc")
+				->limit(1)
+				->fetchAll()[0]['id'];
+
+			db()
+				->insert('apikey')
+				->params(
+					[
+						"name" => $request['name'],
+						'"projectId"' => $lastId,
+						'"userId"' => $request['ownerId']
+					]
+				)
+				->execute();
+
+			$apiKey = db()
+				->select('apikey', 'id')
+				->where('"projectId"', $lastId)
+				->orderBy("created_at", "DESC")
+				->limit(1)
+				->fetchAll()[0]['id'];
+
+			response()->json(
+				[
+					"message" => "Project created successfully",
+					"projectId" => $lastId,
+					"apiKey" => $apiKey
+				], 201, true
+			);
+		});
+
+		app()->get('/projects', function(){
+			$bearer = Leaf\Http\Headers::get("Authorization"); // user id
+			$id = substr($bearer, 7);
+
+			$projects = db()
+				->select('project', '"id", "name", "prefix", "created_at"')
+				->where('"ownerId"', $id)
+				->fetchAll();
+
+			response()->json($projects);
+		});
+
+		app()->post('updateFields', function(){
+			$request = request()->get(['projectId', 'fields']);
+
+			db()
+				->update('project')
+				->params([
+					"fields" => json_encode($request['fields'])
+				])
+				->where('id', $request['projectId'])
+				->execute();
+			
+			response()->json(
+				[
+					"message" => "Fields updated successfully"
+				], 200, true
+			);
+		});
+
+		app()->delete('/deleteProject', function(){
+			$id = request()->get('id');
+
+			db()
+				->delete('project')
+				->where('id', $id)
+				->execute();
+
+			db()
+				->delete('apikey')
+				->where('"projectId"', $id)
+				->execute();
+
+			db()
+				->delete('submission')
+				->where('"projectId"', $id)
+				->execute();
+			
+			response()->json(
+				[
+					"message" => "Project deleted successfully"
+				], 200, true
+			);
+		});
 	});
 
 	app()->group('/submissions', function(){
@@ -37,6 +153,30 @@ app()->group('/v1', function(){
 				->fetchAll();
 
 			response()->json($projects);
+		});
+
+		app()->post('/getSubmission', function(){
+			$request = request()->get(['id']);
+
+			$submission = db()
+				->select('submission')
+				->find($request['id']);
+
+			response()->json($submission);
+		});
+
+		app()->post('/toggleFavorite', function(){
+			$request = request()->get(['submissionId', 'favorite']);
+
+			db()
+  				->update("submission")
+  				->params(["favorite" => $request['favorite']])
+  				->where("id", $request['submissionId'])
+  				->execute();
+
+			response()->json(
+				200, true
+			);
 		});
 
 		app()->post('/submit', function(){
@@ -54,37 +194,62 @@ app()->group('/v1', function(){
 				->select('apikey')
 				->find($key);
 
+			if (!$keyDetails) {
+				response()->exit(
+					[
+						"message" => "Invalid API key"
+					], 401
+				);
+			}
+
 			$fields = db()
 				->select('project', 'fields')
 				->find($keyDetails["projectId"]);
 
-			// check through the fields and validate the data
-			// if the data is valid, save it to the database
-			// else, return an error
-
 			$decodedFields = json_decode($fields['fields'], true);
 
+			$required = [];
+			$empty = [];
+
 			foreach ($decodedFields as $field) {
-				if (!isset($request[$field['name']])) {
-					// Return an error message or throw an exception
-					echo "Field " . $field['name'] . " is required";
+				if ($field['required'] && !isset($request[$field['name']])) {
+					$required[] = "Field '" . $field['name'] . "' is required";
+					continue;
+				}
+				if (gettype($request[$field['name']]) != $field['type']) {
+					$empty[] = "Field '" . $field['name'] . "' is not of type " . $field['type'];
 				}
 			}
+
+			if(count($empty) > 0) {
+				response()->exit(
+					$empty, 400
+				);
+			};
+
+			if(count($required) > 0) {
+				response()->exit(
+						$required, 400
+				);
+			};
+
+			db()
+				->insert('submission')
+				->params(
+					[
+						'"projectId"' => $keyDetails["projectId"],
+						'"userId"' => $keyDetails["userId"],
+						"data" => json_encode($request)
+					]
+				)
+				->execute();
 			
 
-			// response()->json(
-			// 	[
-			// 		// "auth" => $auth,
-			// 		// "decoded" => $decoded,
-			// 		// "username" => $username,
-			// 		// "password" => $password
-			// 		"request" => $request,
-			// 		"auth key" => $key,
-			// 		"key" => $keyDetails,
-			// 		"fields" => $fields,
-			// 		"decodedFields" => $decodedFields
-			// 	]
-			// );
+			response()->json(
+				[
+					"message" => "Submission successful"
+				], 201, true
+			);
 
 		});
 	});
